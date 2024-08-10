efd67565ae60ae656033841fc58540941cac1c34
A_Nama <145112688+A-Nama@users.noreply.github.com>
2024-08-11 01:04:46 +0530

    Update geocoding.py

--- app/geocoding.py
+++ app/geocoding.py
@@ -1,29 +1,17 @@
 import requests
+import certifi
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from a .env file
 load_dotenv()
 
-# Retrieve the API key from environment variables
-GEOCODING_API_KEY = os.getenv('GEOCODING_API_KEY')
+GEOCODING_API_KEY = os.getenv('AIzaSyDP7b0pFz-yQP2aRq-AGPm2EXvjHpFruR8')
 
 def geocode_address(address):
-    # Send a GET request to the Google Maps Geocoding API
-    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GEOCODING_API_KEY}')
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
-        if data['status'] == 'OK':
-            location = data['results'][0]['geometry']['location']
-            return location['lat'], location['lng']
-        else:
-            raise Exception(f"Geocoding API error: {data['status']}")
+    response = requests.get(f'<http://maps.googleapis.com/maps/api/geocode/json?address={address}&key={'AIzaSyDP7b0pFz-yQP2aRq-AGPm2EXvjHpFruR8'}>', verify = certifi.where())
+    data = response.json()
+    if data['status'] == 'OK':
+        location = data['results'][0]['geometry']['location']
+        return location['lat'], location['lng']
     else:
-        # Handle HTTP request errors
-        raise Exception(f"HTTP request failed with status code {response.status_code}")
-
-# Example usage
-# lat, lng = geocode_address('1600 Amphitheatre Parkway, Mountain View, CA')
-# print(f'Latitude: {lat}, Longitude: {lng}')
+        raise Exception("Geocoding API error")
