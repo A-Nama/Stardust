6660583755cfa9700893f6ab01459cd32ecc214a
A_Nama <145112688+A-Nama@users.noreply.github.com>
2024-08-11 06:48:34 +0530

    Update geocoding.py

--- app/geocoding.py
+++ app/geocoding.py
@@ -8,7 +8,8 @@ load_dotenv()
 GEOCODING_API_KEY = os.getenv('AIzaSyDP7b0pFz-yQP2aRq-AGPm2EXvjHpFruR8')
 
 def geocode_address(address):
-    response = requests.get(f'<http://maps.googleapis.com/maps/api/geocode/json?address={address}&key={'AIzaSyDP7b0pFz-yQP2aRq-AGPm2EXvjHpFruR8'}>', verify = certifi.where())
+    api_key=os.getenv("GEOCODING_API_KEY")
+    response = requests.get(f'http://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}', verify = certifi.where())
     data = response.json()
     if data['status'] == 'OK':
         location = data['results'][0]['geometry']['location']
